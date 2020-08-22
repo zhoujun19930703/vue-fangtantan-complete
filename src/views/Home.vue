68143bd2f2f863a2d3dbdba688fe2ba21091f254
zhoujun <zhoujun>
2020-08-22 11:32:39 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="home">
+    <pagesOne v-if="tabIndex == 1" :show.sync="tabIndex"/>
+    <pagesTwo v-if="tabIndex == 2" :show.sync="tabIndex"/>
+    <pagesThree v-if="tabIndex == 3" :choosedList="choosedList" :show.sync="tabIndex"/>
+    <pagesFour v-if="tabIndex == 4" :show.sync="tabIndex"/>
+  </div>
+</template>
+<script>
+  import Bus from '../utils/bus'
+  import pagesOne from './components/pagesOne.vue'
+  import pagesTwo from './components/pagesTwo.vue'
+  import pagesThree from './components/pagesThree.vue'
+  import pagesFour from './components/pagesFour.vue'
+  import {setToken} from '../utils/auth'
+  import store from '../utils/storage'
+
+  export default {
+    name: 'Home',
+    components: {
+      pagesOne,
+      pagesTwo,
+      pagesThree,
+      pagesFour,
+    },
+    data () {
+      return {
+        tabIndex: 0,
+        someList: [], //测试图片
+        choosedList: [] // 喜欢不喜欢勾选的值
+      }
+    },
+    mounted () {
+      Bus.$on('getParams', data => {
+        this.choosedList = data
+      })
+      //获取地址栏的值
+      let data = window.location.search.substr(1)
+      let dataArr = data.split('&')
+      let __dealDataArr = []
+      dataArr.forEach(item =>{
+        let obj= {}
+        obj.key = item.split('=')[0]
+        obj.value = item.split('=')[1]
+        __dealDataArr.push(obj)
+      })
+      console.log(__dealDataArr,'获取数组')
+      let token = 'abd876ad3f6a4108fbd26a27decffca6'
+      console.log(token,'获取token值')
+      setToken(token)
+      store.set('a',JSON.stringify(__dealDataArr))
+      this.tabIndex = 1
+    },
+    methods: {},
+  }
+</script>
+<style lang="scss" scoped>
+.home {
+  width: 750px;
+  height: 100vh;
+  background-color: #171d26;
+  padding: 0px 0px 20px 0px;
+  box-sizing: border-box;
+  position: relative;
+}
+</style>
---
+++ src/views/components/pagesThree.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="content">
+    <span>风格测试结果正在生成...</span>
+    <nprogress
+      :h="10"
+      :w="npgressW"
+      bgc="rgba(255, 255, 255, 0.8)"
+      parentBgc="rgba(245,245,245,.2)"
+      sec="2s"
+    />
+  </div>
+</template>
+<script>
+import nprogress from './nprogress.vue'
+import {api_getTestData} from '../../api'
+
+export default {
+  components: {
+    nprogress,
+  },
+  props: {
+    choosedList: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      npgressW: '0%',
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.npgressW = '100%'
+      this.test(this.choosedList)
+    }, 1000)
+  },
+  methods: {
+    test (data) {
+      this.$ajax(api_getTestData, { result: data }).then((res) => {
+        console.log('测试', res)
+        if (res.code == 200) {
+          setTimeout(_ =>{
+            this.$emit('update:show', 4)
+          },2000)
+        }
+      })
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+.content {
+  span {
+    font-size: 26px;
+    font-family: "PingFangSC-Regular,PingFang SC";
+    color: #ffffff;
+    margin-bottom: 60px;
+  }
+
+  height: 100px;
+  width: 488px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 10px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  overflow: hidden;
+}
+</style>
